47ef25c0c939867c8a542c248d0bd078780ee4ce
= <=>
2023-08-12 17:28:48 +0000

    reusable Banners

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerCardsList} = props
+  const {className, headerText, description} = bannerCardsList
+  return (
+    <li className={className}>
+      <h1>{headerText}</h1>
+      <p>{description}</p>
+      <button type="button">Show More</button>
+    </li>
+  )
+}
+
+export default BannerCardItem
